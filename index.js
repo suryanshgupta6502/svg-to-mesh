219a72b48336cff0c0a3376202ac352154f4e3e8
Suryansh <suryanshgupta6502@gmail.com>
2025-02-18 21:54:16 +0530

    remove extra files

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import * as THREE from 'three';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
 
-export async function svgToMesh(url, options = { depth: 50, color: 0x00ffff , position: new THREE.Vector3(0, 1, 0) }) {
+export async function svgToMesh(url, options = { depth, color, position, scale }) {
     try {
+        console.log(options);
+        // console.log(optionss);
+        const {
+            depth = 50,
+            color = 0xffffff,
+            position = new THREE.Vector3(0, 1, 0),
+            scale = new THREE.Vector3(1, 1, 1),
+        } = options
+
+
         const loader = new SVGLoader();
         const data = await loader.loadAsync(url);
 
@@ -11,12 +21,12 @@ export async function svgToMesh(url, options = { depth: 50, color: 0x00ffff , po
             const shapes = SVGLoader.createShapes(path);
             for (const shape of shapes) {
                 const geometry = new THREE.ExtrudeGeometry(shape, {
-                    depth: options.depth,
+                    depth: depth,
                     bevelEnabled: false,
                 });
 
                 const material = new THREE.MeshStandardMaterial({
-                    color: options.color,
+                    color: color,
                     side: THREE.DoubleSide,
                 });
 
@@ -38,7 +48,9 @@ export async function svgToMesh(url, options = { depth: 50, color: 0x00ffff , po
         // console.log(position);
         // console.log(center, group.position, group.position.sub(center));
         // console.log(group.position);
-        group.position.add(options.position)
+        console.log(depth);
+        group.position.add(position)
+        group.scale.set(scale.x, scale.y, scale.z)
         // console.log(group.position);
 
 
